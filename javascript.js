23e7d5a78fe507bb503262c802575b25214f2513
RyryZA <134719754+RyryZA@users.noreply.github.com>
2025-10-05 15:51:27 +0200

    Created computer choice logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+
+function getComputerChoice () {
+    let num = Math.random();
+
+    if (num > 0.66) {
+        return 'Rock'
+    } else if (num < 0.66 && num > 0.33) {
+        return 'Paper'
+    } else {
+        return 'Scissors'
+    }
+    //returns the play as either rock, paper or scissors
+};
+
+
+console.log (getComputerChoice());
